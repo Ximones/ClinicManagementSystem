ec00b568341a27e3fc7b5728aef6e5e1e33141f5
Ximones <simonchok1124@gmail.com>
2025-07-12 03:37:09 +0800

    Added UI and Image Util
    1. Added UI
    - Main frame, Main menu UI, Doctor's UI,   and Doctor's functionality
    
    2. Image Util
    - get image from directory

---
+++ src/utility/ImageUtils.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author deadb
+ */
+public class ImageUtils {
+
+    // A private, static field to hold the cached logo image.
+    private static ImageIcon imageIcon;
+    private static JLabel imageLabel;
+    private static final String IMAGE_DIR = "/resources/";
+
+    public static JLabel getImageLabel(String imageName,JLabel jLabel) {
+
+        imageLabel = jLabel;
+        
+        if (imageLabel != null) {
+            try {
+                imageLabel.setIcon(getLogoIcon(IMAGE_DIR+imageName, 100));
+                imageLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+                imageLabel.setBorder(new javax.swing.border.EmptyBorder(10, 0, 0, 0));
+            } catch (Exception ex) {
+                imageLabel.setText("Logo not found!");
+            }
+        }
+
+        return imageLabel;
+    }
+
+    /**
+     * Gets the shared logo ImageIcon. The image is loaded from the disk only
+     * the first time this method is called. Subsequent calls return the cached
+     * instance.
+     *
+     * @param imageDir
+     * @param imageSize
+     * @return The ImageIcon for the logo.
+     */
+    public static ImageIcon getLogoIcon(String imageDir, int imageSize) {
+        // Check if the icon has been loaded yet
+        if (imageIcon == null) {
+            // If not, load it from the disk
+            try {
+                ImageIcon originalIcon = new ImageIcon(ImageUtils.class.getResource(imageDir));
+                Image resizedImage = originalIcon.getImage().getScaledInstance(-1, imageSize, Image.SCALE_SMOOTH);
+                imageIcon = new ImageIcon(resizedImage); // Store the resized icon in our static field
+            } catch (Exception ex) {
+                System.err.println("Image not found: " + ex.getMessage());
+                // Optionally return a placeholder or null
+            }
+        }
+        // Return the cached icon
+        return imageIcon;
+    }
+}
